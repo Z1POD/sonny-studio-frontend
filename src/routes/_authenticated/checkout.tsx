7e064aa0244cfa3534c614766203ab0302c05e6e
natty4 <natty7kt@gmail.com>
2026-06-26 22:17:07 +0300

    feat: improper directory on routers

--- src/routes/_authenticated/checkout.tsx
+++ src/routes/_authenticated/checkout.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { CheckOut } from '@/features/catalog/components/CheckOut'
+import { CheckOut } from '@/features/checkout/components/CheckOut'
 
 export const Route = createFileRoute('/_authenticated/checkout')({
   component: RouteComponent,
